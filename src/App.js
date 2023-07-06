411e458e1f0db3f23b332042d8b038be166f44ce
sayanta2005 <92053478+sayanta2005@users.noreply.github.com>
2023-07-06 17:54:49 +0530

    Created Tests and Codes according to tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+/*
+function Square({ value, onSquareClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="square" data-testid="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+*/
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (squares[i]) {
+      return;
+    }
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setXIsNext(!xIsNext);
+    setSquares(nextSquares);
+  }
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "The Winner is: " + winner;
+  }
+   else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
+  return (
+    <section>
+      <div className="status" data-testid="status">{status}</div>
+      <div className="board-row" data-testid="board-row0">
+        <button className="square" data-testid="square0" value={squares[0]} onClick={() => handleClick(0)}>{squares[0]}</button>
+        <button className="square" data-testid="square1" value={squares[1]} onClick={() => handleClick(1)}>{squares[1]}</button>
+        <button className="square" data-testid="square2" value={squares[2]} onClick={() => handleClick(2)}>{squares[2]}</button>
+      </div>
+      <div className="board-row" data-testid="board-row1">
+        <button className="square" data-testid="square3" value={squares[3]} onClick={() => handleClick(3)}>{squares[3]}</button>
+        <button className="square" data-testid="square4" value={squares[4]} onClick={() => handleClick(4)}>{squares[4]}</button>
+        <button className="square" data-testid="square5" value={squares[5]} onClick={() => handleClick(5)}>{squares[5]}</button>
+      </div>
+      <div className="board-row" data-testid="board-row2">
+        <button className="square" data-testid="square6" value={squares[6]} onClick={() => handleClick(6)}>{squares[6]}</button>
+        <button className="square" data-testid="square7" value={squares[7]} onClick={() => handleClick(7)}>{squares[7]}</button>
+        <button className="square" data-testid="square8" value={squares[8]} onClick={() => handleClick(8)}>{squares[8]}</button>
+      </div>
+    </section>
   );
 }
 
-export default App;
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,146 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Board component", () => {
+  it("renders without crashing", () => {
+    render(<App />);
+  });
+  it("should render 3 rows",()=>{
+    const board = render(<App />);
+    const row0 = board.getByTestId("board-row0");
+    const row1 = board.getByTestId("board-row1");
+    const row2 = board.getByTestId("board-row2");
+    expect(row0).toBeInTheDocument();
+    expect(row1).toBeInTheDocument();
+    expect(row2).toBeInTheDocument();
+  });
+  it("should render 9 null squares",()=>{
+    const board = render(<App />);
+    const square0 = board.getByTestId("square0");
+    expect(square0).toHaveTextContent("");
+    const square1 = board.getByTestId("square1");
+    expect(square1).toHaveTextContent("");
+    const square2 = board.getByTestId("square2");
+    expect(square2).toHaveTextContent("");
+    const square3 = board.getByTestId("square3");
+    expect(square3).toHaveTextContent("");
+    const square4 = board.getByTestId("square4");
+    expect(square4).toHaveTextContent("");
+    const square5 = board.getByTestId("square5");
+    expect(square5).toHaveTextContent("");
+    const square6 = board.getByTestId("square6");
+    expect(square6).toHaveTextContent("");
+    const square7 = board.getByTestId("square7");
+    expect(square7).toHaveTextContent("");
+    const square8 = board.getByTestId("square8");
+    expect(square8).toHaveTextContent("");
+  })
+  it("Should render the next player as X initially",()=>{
+    render(<App />);
+    expect(screen.getByTestId("status")).toHaveTextContent("Next Player: X");
+  })
 });
+
+describe("Check Functionalities",()=>{
+  it("Should have X on first click",()=>{
+    render(<App />);
+    const square0 = screen.getByTestId("square0");
+    fireEvent.click(square0);
+    expect(square0).toHaveTextContent("X");
+  });
+  it("Should render next player as O after first click",()=>{
+    render(<App />);
+    fireEvent.click(screen.getByTestId("square0"));
+    expect(screen.getByTestId("status")).toHaveTextContent("Next Player: O");
+  })
+  it("Should have O on second click",()=>{
+    render(<App />);
+    const square0 = screen.getByTestId("square0");
+    fireEvent.click(square0);
+    const square1 = screen.getByTestId("square1");
+    fireEvent.click(square1);
+    expect(square1).toHaveTextContent("O");
+  });
+  it("Should have X on odd number of clicks",()=>{
+    render(<App />);
+    fireEvent.click(screen.getByTestId("square0"));
+    fireEvent.click(screen.getByTestId("square1"));
+    fireEvent.click(screen.getByTestId("square2"));
+    fireEvent.click(screen.getByTestId("square3"));
+    const square4 = screen.getByTestId("square4");
+    fireEvent.click(square4);
+    expect(square4).toHaveTextContent("X");
+  });
+  it("should have O on even number of clicks",()=>{
+    render(<App />);
+    fireEvent.click(screen.getByTestId("square0"));
+    fireEvent.click(screen.getByTestId("square1"));
+    fireEvent.click(screen.getByTestId("square2"));
+    fireEvent.click(screen.getByTestId("square3"));
+    fireEvent.click(screen.getByTestId("square4"));
+    const square5 = screen.getByTestId("square5");
+    fireEvent.click(square5);
+    expect(square5).toHaveTextContent("O");
+  });
+})
+
+describe("Declaration of Winner",()=>{
+  it("Should declare X as an winner if X completes a row first before O",()=>{
+    render(<App />);
+    fireEvent.click(screen.getByTestId("square0"));
+    fireEvent.click(screen.getByTestId("square3"));
+    fireEvent.click(screen.getByTestId("square1"));
+    fireEvent.click(screen.getByTestId("square4"));
+    fireEvent.click(screen.getByTestId("square2"));
+    expect(screen.getByTestId("status")).toHaveTextContent("The Winner is: X");
+  });
+  it("Should declare O as an winner if O completes a row first before X",()=>{
+    render(<App />);
+    fireEvent.click(screen.getByTestId("square0"));
+    fireEvent.click(screen.getByTestId("square3"));
+    fireEvent.click(screen.getByTestId("square1"));
+    fireEvent.click(screen.getByTestId("square4"));
+    fireEvent.click(screen.getByTestId("square8"));
+    fireEvent.click(screen.getByTestId("square5"));
+    expect(screen.getByTestId("status")).toHaveTextContent("The Winner is: O");
+  })
+  it("Should declare X as an winner if X completes a column before O",()=>{
+    render(<App />);
+    fireEvent.click(screen.getByTestId("square0"));
+    fireEvent.click(screen.getByTestId("square1"));
+    fireEvent.click(screen.getByTestId("square3"));
+    fireEvent.click(screen.getByTestId("square4"));
+    fireEvent.click(screen.getByTestId("square6"));
+    expect(screen.getByTestId("status")).toHaveTextContent("The Winner is: X");
+  })
+  it("Should declare O as an winner if O completes a column before X",()=>{
+    render(<App />);
+    fireEvent.click(screen.getByTestId("square0"));
+    fireEvent.click(screen.getByTestId("square1"));
+    fireEvent.click(screen.getByTestId("square3"));
+    fireEvent.click(screen.getByTestId("square4"));
+    fireEvent.click(screen.getByTestId("square5"));
+    fireEvent.click(screen.getByTestId("square7"));
+    expect(screen.getByTestId("status")).toHaveTextContent("The Winner is: O");
+  });
+  it("Should Declare X if it completes a diagonal before O",()=>{
+    render(<App />);
+    fireEvent.click(screen.getByTestId("square0"));
+    fireEvent.click(screen.getByTestId("square1"));
+    fireEvent.click(screen.getByTestId("square4"));
+    fireEvent.click(screen.getByTestId("square2"));
+    fireEvent.click(screen.getByTestId("square8"));
+    expect(screen.getByTestId("status")).toHaveTextContent("The Winner is: X");
+  })
+  it("Should Declare O if it completes a diagonal before X",()=>{
+    render(<App />);
+    fireEvent.click(screen.getByTestId("square1"));
+    fireEvent.click(screen.getByTestId("square0"));
+    fireEvent.click(screen.getByTestId("square2"));
+    fireEvent.click(screen.getByTestId("square4"));
+    fireEvent.click(screen.getByTestId("square5"));
+    fireEvent.click(screen.getByTestId("square8"));
+    expect(screen.getByTestId("status")).toHaveTextContent("The Winner is: O");
+  })
+})
